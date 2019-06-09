d4f26ae83b203f4bd437b81b42fc2106c206531b
INDIA GUY <rguy129@yahoo.com>
2019-06-09 00:24:26 -0400

    changed proxy back to 3001

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/', routes)
 // io.listen(port);
 // console.log('socket io is listening on port ', port);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log("We up in here on " + PORT);

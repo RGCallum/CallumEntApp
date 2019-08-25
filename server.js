e4e1fba8dd945cfbe7f1e4f5fb74d96b0b22750f
INDIA GUY <rguy129@yahoo.com>
2019-08-25 17:08:53 -0400

    chg .env port to 5000 server.js

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ app.get('/', (req, res) => {
 app.use('/', routes);
 
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log("We up in here on " + PORT);

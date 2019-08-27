1b094c841784c3c593fed68d3c914911c25c5fd5
INDIA GUY <rguy129@yahoo.com>
2019-08-27 01:33:25 -0400

    fit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -17,26 +17,19 @@ font-weight: 700;
 text-shadow: 1px 1px 10px rgb(28, 147, 145);
 align-items: center;
 justify-content: center;
-// background-color: #587d9689;
 z-index: 1;
+
+background-image: url('/images/office.jpeg');
+background-repeat: no-repeat;
+background-position: center;
 background-attachment: fixed;
 background-size: cover;
-background-image: url('/images/office.jpeg');
-img{
-    
-  position: absolute;
-    z-index: -2;
-    //  width: 150%;
-     background-size: cover;
-     background-repeat: no-repeat;
-    margin-top: -100px;
-  
-}
+
 `
 
 const CountStyles = styled.div`
     z-index: 1;
-    height: 100vw;
+    // height: 100vw;
     text-align: center;
     
 `
@@ -89,6 +82,10 @@ class Home extends Component {
                             onFailure={responseGoogle}
                             cookiePolicy={'single_host_origin'} />
 
+<br/>
+<br/>
+<br/>
+<br/>
 
                     </CountStyles>
                     {/* <img src="/images/CAL_ent_logo.png" alt="logo" /> */}
